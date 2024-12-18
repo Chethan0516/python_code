1a9bc78410873689aa1ad9deaa93511acc172b1c
Chethan0516 <103435265+Chethan0516@users.noreply.github.com>
2024-12-18 19:47:54 +0530

    Update hash_small.py

--- hash/hash_small.py
+++ hash/hash_small.py
@@ -1,12 +1,1 @@
-print("this is a feature branch")
 
-print("this is in main") # without conflict
-
-print("from main in one") # without conflict
-
-print("for conflict") # forconflict
-
-print("in Two branch")
-print("for conflict") # forconflict main
-
-print("this is to check new feature")
